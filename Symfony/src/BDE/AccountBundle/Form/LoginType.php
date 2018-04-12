3bc4cb100f504aff29d8a741951ab57e9009007d
CaSSoSSiSSe <yohann.neraud@viacesi.fr>
2018-04-12 10:19:49 +0200

    Login and Register Complete

--- Symfony/src/BDE/AccountBundle/Form/LoginType.php
+++ Symfony/src/BDE/AccountBundle/Form/LoginType.php
@@ -9,6 +9,7 @@
 namespace BDE\AccountBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,7 +19,8 @@ class LoginType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('email');
+        $builder->add('email', EmailType::class, array(
+        ));
         $builder->add('password', PasswordType::class, array(
         ));
     }
