002d0a57cce4f059f0ded8449d39c158956a2438
CaSSoSSiSSe <yohann.neraud@viacesi.fr>
2018-04-16 16:37:17 +0200

    Command complete

--- Symfony/src/BDE/ShopBundle/Form/ArticleType.php
+++ Symfony/src/BDE/ShopBundle/Form/ArticleType.php
@@ -3,19 +3,21 @@
 namespace BDE\ShopBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class CartType extends AbstractType
+class ArticleType extends AbstractType
 {
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('add', SubmitType::class, array('label' => 'Commander'));
+        $builder->add('quantity', ChoiceType::class);
+        $builder->add('add', SubmitType::class, array('label' => 'Ajouter au panier'));
     }
 
     public function getName()
     {
-        return 'command';
+        return 'addcart';
     }
 }
